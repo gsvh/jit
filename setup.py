b5521d3b98d32951ba999c561f13c174381d29d2
gsvh <georgesebastiaan.vh@gmail.com>
2024-06-30 12:34:30 +0200

    refactor(setup): extract dependencies to requirements.txt file

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
 
+
+# Function to read requirements from a file
+def load_requirements(filename='requirements.txt'):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line.strip()]
+
 setup(
     name='jit',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'click',
-        'GitPython',
-    ],
+    install_requires=load_requirements(),
     entry_points={
         'console_scripts': [
             'jit=jit.cli:jit',
