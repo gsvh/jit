afacc472b0ab0fccc51bfb953128124a5dd1c405
vloermatt <mdldavies@gmail.com>
2024-07-01 10:50:20 +0200

    feat(cli): add update command

--- jit/cli.py
+++ jit/cli.py
@@ -1,7 +1,8 @@
 import logging
 import os
-
+import subprocess
 import click
+import shutil
 import git
 from rich.logging import RichHandler
 
@@ -88,3 +89,58 @@ def config(repo_name):
         repo = git.Repo(repo_path)
         repo_name = repo.remotes.origin.url.split('/')[-1].replace('.git', '')
     update_config(repo_name)
+
+@jit.command()
+def update():
+    """Fetch the latest changes from the repo and re-install jit."""
+    logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+    log = logging.getLogger("rich")
+
+    repo_path = os.getcwd()
+    repo = git.Repo(repo_path)
+    branch = repo.active_branch
+
+    log.info(f"Current branch: {branch.name}")
+
+    # Check if the branch has an upstream set
+    if not branch.tracking_branch():
+        # Set upstream to the default remote branch (origin/main or origin/master)
+        try:
+            remote_branch = repo.remotes.origin.refs.main
+        except AttributeError:
+            remote_branch = repo.remotes.origin.refs.master
+
+        log.info(f"Setting upstream to {remote_branch}")
+        repo.git.branch('--set-upstream-to', remote_branch, branch.name)
+
+    log.info("Fetching the latest changes from the repository...")
+    repo.git.pull()
+
+    log.info("Re-installing the tool using the Makefile...")
+    env = os.environ.copy()
+    env["COLUMNS"] = str(shutil.get_terminal_size().columns)
+    env["PYTHONUNBUFFERED"] = "1"  # Ensures that Python output is not buffered
+    env["FORCE_COLOR"] = "1"  # Force enable ANSI colors
+
+    process = subprocess.Popen(
+        ["make", "re-install"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        env=env  # Pass the modified environment
+    )
+
+    # Wait for the process to complete and get the return code
+    return_code = process.wait()
+
+    if return_code == 0:
+        # Log stdout and stderr directly after waiting for the process to complete
+        stdout, stderr = process.communicate()
+        log.info(stdout)
+        log.info("jit been updated and re-installed successfully 🎉")
+    else:
+        # Log stderr directly after waiting for the process to complete
+        stdout, stderr = process.communicate()
+        log.error(stderr)
+        log.error("An error occurred while re-installing the tool.")        
+
