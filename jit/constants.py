a3a7bf21c2cb74828a691fcdb28eb716364fd60c
George van Heerden <george.vanheerden@sovtech.com>
2024-07-03 08:17:32 +0200

    refactor(constants): simplify commit message specifications & combine with commit message template

--- jit/constants.py
+++ jit/constants.py
@@ -12,30 +12,15 @@ PR_TEMPLATE = """
 * {a list of the changes made}
 """
 
-# https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commit-message-format
-COMMIT_MESSAGE_TEMPLATE = """
-<type>(<scope>): <subject>
-"""
-
 # https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#type
-COMMIT_MESSAGE_SPECIFICATION = """
-Type
-Must be one of the following:
--feat: A new feature
--fix: A bug fix
--docs: Documentation only changes
--style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
--refactor: A code change that neither fixes a bug nor adds a feature
--perf: A code change that improves performance
--test: Adding missing or correcting existing tests
--chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+COMMIT_MESSAGE_SPECIFICATION = """Template:
+Template:
+`<type>(<scope>): <subject>` or `<type>: <subject>`
 
-Scope
-The scope could be anything specifying place of the commit change. For example $location, $browser, $compile, $rootScope, ngHref, ngClick, ngView, etc..
-
-Subject
-The subject contains succinct description of the change:
-use the imperative, present tense: "change" not "changed" nor "changes"
-don't capitalize first letter
-no dot (.) at the end
+Specifications:
+- `scope` (optional): Specifies the place of the commit, e.g.,$file_name, $location, $browser.
+- `subject`: Describes the change using:
+  - Imperative mood: "change", not "changed" or "changes".
+  - Lowercase initial letter.
+  - No period (.) at the end.
 """
