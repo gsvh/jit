b61f0e2c852f1fa3c93a7bdaf6a4e6a5ef9b95a0
gsvh <georgesebastiaan.vh@gmail.com>
2024-07-28 20:05:22 +0200

    chore: bump setup.py version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='jit-cli',
-    version='0.1.1',
+    version='0.1.2',
     description='A command line tool to automatically create pull requests on GitHub',
     long_description=long_description,
     long_description_content_type="text/markdown",
